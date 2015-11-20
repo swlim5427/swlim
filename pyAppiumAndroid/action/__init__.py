c7fbf31a23153a6146918c12c3aeef4f5189c317
swlim <18358898@qq.com>
2015-11-20 17:59:41 +0800

    add home_activity

--- pyAppiumAndroid/action/__init__.py
+++ pyAppiumAndroid/action/__init__.py
@@ -1,4 +1,5 @@
 __all__ = ["tag_home_activity",
            "tag_home_announcement",
            "tag_message_cloudcardlist",
-           "tag_message_wxy"]
+           "tag_message_wxy",
+           "tag_home_weeklydiet"]
--- pyAppiumAndroid/action/screenshot.py
+++ pyAppiumAndroid/action/screenshot.py
@@ -1,4 +1,29 @@
 # -*- coding: utf-8 -*-
+import time
+import logging
 
 def screenshot(driver,path):
     driver.get_screenshot_as_file(path)
+
+def backButton(driver,picFlile,picName,funcName):
+    try:
+        wxyDetailsBack = driver.find_element_by_name("返 回")
+        wxyDetailsBack.click()
+        time.sleep(1)
+        path = picFlile+picName
+        screenshot(driver,path)
+        logging.info(funcName+u"返回成功")
+        time.sleep(1)
+    except:
+        logging.error(funcName+u"返回失败")
+
+def inTagHome(driver,picFlile,picName):
+    try:
+        wxy_jiayuan = driver.find_element_by_name("家园")
+        wxy_jiayuan.click()
+        time.sleep(2)
+        screenshot(driver,picFlile+picName)
+        time.sleep(2)
+        logging.info(u"进入家园选项卡成功")
+    except:
+        logging.error(u"进入家园选项卡失败")
