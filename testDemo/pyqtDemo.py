7be6996aafca3a348268d6217397650a9fde8da4
swlim <18358898@qq.com>
2015-11-27 15:13:23 +0800

    optimization

--- testDemo/pyqtDemo.py
+++ testDemo/pyqtDemo.py
@@ -1,9 +1,40 @@
 import sys
 from PyQt4 import QtGui
 import time
+import datetime
+import StringIO
+import cStringIO
+
+
+def timeAction():
+
+    inputYear = datetime.datetime.now().year
+    inputMonth = datetime.datetime.now().month
+    inputDay = datetime.datetime.now().day
+    inputHour = datetime.datetime.now().hour
+    inputMinute = datetime.datetime.now().minute
+    inputSecond = datetime.datetime.now().second
+
+    mTime = datetime.datetime.now().microsecond
+    imputDateTime = datetime.datetime(year=int(inputYear),month=int(inputMonth),day=int(inputDay),
+                                      hour=int(inputHour),minute=int(inputMinute),second=int(inputSecond))
+    formatTime = long(round(time.mktime(imputDateTime.timetuple())))
+    nowTime =  long(str(formatTime)+str(mTime/1000))
+    return nowTime
+
+startTime = timeAction()
+
+timeInIO = StringIO.StringIO(startTime)
+#timeInIO.write(startTime)
+time.sleep(2)
+timeOutIO = timeInIO.getvalue()
+print timeAction()
+print timeOutIO
+timeInIO.close()
 
 
 
+'''
 app = QtGui.QApplication(sys.argv)
 
 widget = QtGui.QWidget()
@@ -11,3 +42,4 @@ widget.resize(250, 150)
 widget.setWindowTitle('swlim')
 widget.show()
 sys.exit(app.exec_())
+'''
