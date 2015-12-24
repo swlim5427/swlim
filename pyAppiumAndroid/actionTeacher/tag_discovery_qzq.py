669acf7039476da673d35488e2f1178e82dd6470
swlim <18358898@qq.com>
2015-12-24 17:52:48 +0800

    add discovery

--- pyAppiumAndroid/actionTeacher/tag_discovery_qzq.py
+++ pyAppiumAndroid/actionTeacher/tag_discovery_qzq.py
@@ -10,11 +10,32 @@ def tagDiscoveryQzq(driver,picFlile):
         inTagDiscoveryAtion = inTagDiscovery(driver,picFlile,picName_in,funcName)
     if inTagDiscoveryAtion == 1:
         try:
-            driver.find_element_by_name('活动').click()
+            driver.find_element_by_name('亲子圈').click()
             time.sleep(2)
             path = picFlile+u"发现_亲子圈列表.png"
             screenshot(driver,path)
             logging.info(u"进入发现-亲子圈成功")
             time.sleep(1)
+            try:
+                driver.find_element_by_id("com.tuxing.app.teacher:id/ll_portrait").click()
+                time.sleep(2)
+                path = picFlile+u"发现_亲子圈_教师头像进入.png"
+                screenshot(driver,path)
+                logging.info(u"进入发现-教师头像进入成功")
+                try:
+                    driver.find_element_by_id("com.tuxing.app.teacher:id/ll_right").click()
+                    time.sleep(1)
+                    path = picFlile+u"发现_亲子圈_教师头像_消息列表.png"
+                    screenshot(driver,path)
+                    logging.info(u"进入发现-教师头像-进入消息列表成功")
+
+                except:
+                    logging.error(u"进入发现-教师头像-进入消息列表失败  ")
+            except:
+                logging.error(u"进入发现-教师头像进入失败")
+
+
+
+
         except:
-            logging.info(u"进入发现-亲子圈失败")
+            logging.error(u"进入发现-亲子圈失败")
