2b2d371a7a6ce8cc739a65ccad39d119f5f2f700
swlim <18358898@qq.com>
2016-01-19 15:50:15 +0800

    add parent app

--- pyAppiumAndroid/action/tag_home_mychildattendance.py
+++ pyAppiumAndroid/action/tag_home_mychildattendance.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from pubaction import *
 
 def tatMyChildAttendance(actionTypeMessage):
@@ -5,7 +6,7 @@ def tatMyChildAttendance(actionTypeMessage):
     picFlile = actionTypeMessage["picFlile"]
     homeIconType = int(actionTypeMessage["homeIconType"])
     appType = actionTypeMessage["appType"]
-    funcName = "±¦±¦¿¼ÇÚ_"
+    funcName = "å®å®è€ƒå‹¤_"
 
     if checkTag(driver) == "home":
         inTagHomeAtion = 1
@@ -17,71 +18,71 @@ def tatMyChildAttendance(actionTypeMessage):
         try:
             driver.find_elements_by_id("com.tuxing.app.teacher:id/home_item_icon")[homeIconType].click()
             time.sleep(2)
-            screenshot(driver,picFlile+u"¼ÒÔ°_½øÈë±¦±¦¿¼ÇÚ.png")
-            logging.info(u"½øÈë±¦±¦¿¼ÇÚ³É¹¦")
+            screenshot(driver,picFlile+u"å®¶å›­_è¿›å…¥å®å®è€ƒå‹¤.png")
+            logging.info(u"è¿›å…¥å®å®è€ƒå‹¤æˆåŠŸ")
             time.sleep(2)
             try:
                 driver.find_element_by_id("com.tuxing.app.home:id/prevMonth").click()
                 time.sleep(2)
-                screenshot(driver,picFlile+u"±¦±¦¿¼ÇÚ-ÉÏÒ»ÔÂ.png")
-                logging.info(u"±¦±¦¿¼ÇÚ-·­ÉÏÒ»ÔÂ³É¹¦")
+                screenshot(driver,picFlile+u"å®å®è€ƒå‹¤-ä¸Šä¸€æœˆ.png")
+                logging.info(u"å®å®è€ƒå‹¤-ç¿»ä¸Šä¸€æœˆæˆåŠŸ")
                 time.sleep(1)
                 try:
                     driver.find_element_by_id("com.tuxing.app.home:id/nextMonth").click()
                     time.sleep(2)
-                    screenshot(driver,picFlile+u"±¦±¦¿¼ÇÚ-ÏÂÒ»ÔÂ.png")
-                    logging.info(u"±¦±¦¿¼ÇÚ-·­ÏÂÒ»ÔÂ³É¹¦")
+                    screenshot(driver,picFlile+u"å®å®è€ƒå‹¤-ä¸‹ä¸€æœˆ.png")
+                    logging.info(u"å®å®è€ƒå‹¤-ç¿»ä¸‹ä¸€æœˆæˆåŠŸ")
                     time.sleep(1)
                 except:
-                    logging.info(u"±¦±¦¿¼ÇÚ-·­ÏÂÒ»ÔÂÊ§°Ü")
+                    logging.info(u"å®å®è€ƒå‹¤-ç¿»ä¸‹ä¸€æœˆå¤±è´¥")
             except:
-                logging.error(u"±¦±¦¿¼ÇÚ-·­ÉÏÒ»ÔÂÊ§°Ü")
+                logging.error(u"å®å®è€ƒå‹¤-ç¿»ä¸Šä¸€æœˆå¤±è´¥")
 
             try:
-                driver.find_element_by_name("Ë¢¿¨¼ÇÂ¼")
+                driver.find_element_by_name("åˆ·å¡è®°å½•")
                 try:
                     checkInList = driver.find_elements_by_id("com.tuxing.app.home:id/home_card_head")
                     checkInListPic = driver.find_elements_by_class_name("android.widget.ImageView")
                     checkInListPic[1].click()
-                    path = picFlile+u"±¦±¦¿¼ÇÚ_Ë¢¿¨ÁĞ±í_Í¼Æ¬.png"
+                    path = picFlile+u"å®å®è€ƒå‹¤_åˆ·å¡åˆ—è¡¨_å›¾ç‰‡.png"
                     screenshot(driver,path)
-                    logging.info(u"±¦±¦¿¼ÇÚ_²é¿´Ë¢¿¨ÕÕÆ¬³É¹¦")
+                    logging.info(u"å®å®è€ƒå‹¤_æŸ¥çœ‹åˆ·å¡ç…§ç‰‡æˆåŠŸ")
                     time.sleep(2)
                 except:
-                    logging.error(u"±¦±¦¿¼ÇÚ_²é¿´Ë¢¿¨ÕÕÆ¬Ê§°Ü")
+                    logging.error(u"å®å®è€ƒå‹¤_æŸ¥çœ‹åˆ·å¡ç…§ç‰‡å¤±è´¥")
                 try:
                     driver.find_element_by_id("com.tuxing.app.home:id/wivPhoto").click()
-                    path = picFlile+u"±¦±¦¿¼ÇÚ_ÔÆÎÀÊ¿Ë¢¿¨ÁĞ±í_Í¼Æ¬·µ»Ø.png"
+                    path = picFlile+u"å®å®è€ƒå‹¤_äº‘å«å£«åˆ·å¡åˆ—è¡¨_å›¾ç‰‡è¿”å›.png"
                     screenshot(driver,path)
-                    logging.info(u"±¦±¦¿¼ÇÚ_Ë¢¿¨ÕÕÆ¬·µ»Ø³É¹¦")
+                    logging.info(u"å®å®è€ƒå‹¤_åˆ·å¡ç…§ç‰‡è¿”å›æˆåŠŸ")
                     time.sleep(2)
                 except:
-                    logging.error(u"±¦±¦¿¼ÇÚ_Ë¢¿¨ÕÕÆ¬·µ»ØÊ§°Ü")
+                    logging.error(u"å®å®è€ƒå‹¤_åˆ·å¡ç…§ç‰‡è¿”å›å¤±è´¥")
                 try:
                     for j in range(3):
                         driver.swipe(start_x=313,start_y=1163,end_x=313,end_y=209,duration=600)
                         time.sleep(1)
-                        path = picFlile+u"±¦±¦¿¼ÇÚ-Ë¢¿¨¼ÇÂ¼ÏÂÀ­"+str(j+1)+".png"
+                        path = picFlile+u"å®å®è€ƒå‹¤-åˆ·å¡è®°å½•ä¸‹æ‹‰"+str(j+1)+".png"
                         screenshot(driver,path)
-                        logging.info(u"±¦±¦¿¼ÇÚ-Ë¢¿¨¼ÇÂ¼ÏÂÀ­³É¹¦"+str(j+1))
+                        logging.info(u"å®å®è€ƒå‹¤-åˆ·å¡è®°å½•ä¸‹æ‹‰æˆåŠŸ"+str(j+1))
                         time.sleep(1)
                     for i in range(3):
                         driver.swipe(start_x=313,start_y=209,end_x=313,end_y=1163,duration=600)
                         time.sleep(1)
-                        path = picFlile+u"±¦±¦¿¼ÇÚ-Ë¢¿¨¼ÇÂ¼ÉÏÀ­"+str(i+1)+".png"
+                        path = picFlile+u"å®å®è€ƒå‹¤-åˆ·å¡è®°å½•ä¸Šæ‹‰"+str(i+1)+".png"
                         screenshot(driver,path)
-                        logging.info(u"±¦±¦¿¼ÇÚ-Ë¢¿¨¼ÇÂ¼ÉÏÀ­³É¹¦"+str(i+1))
+                        logging.info(u"å®å®è€ƒå‹¤-åˆ·å¡è®°å½•ä¸Šæ‹‰æˆåŠŸ"+str(i+1))
                         time.sleep(1)
 
                 except:
-                    logging.error(u"±¦±¦¿¼ÇÚ-Ë¢¿¨¼ÇÂ¼ÍÏ¶¯Ê§°Ü")
+                    logging.error(u"å®å®è€ƒå‹¤-åˆ·å¡è®°å½•æ‹–åŠ¨å¤±è´¥")
             except:
-                logging.error(u"½øÈë±¦±¦¿¼ÇÚ-Ë¢¿¨¼ÇÂ¼Ê§°Ü")
+                logging.error(u"è¿›å…¥å®å®è€ƒå‹¤-åˆ·å¡è®°å½•å¤±è´¥")
 
-            picName_list_back = u"±¦±¦¿¼ÇÚ-Ë¢¿¨ÁĞ±í·µ»Ø.png"
-            backButton(driver,picFlile,picName_list_back,funcName+"Ë¢¿¨ÁĞ±í_")
+            picName_list_back = u"å®å®è€ƒå‹¤-åˆ·å¡åˆ—è¡¨è¿”å›.png"
+            backButton(driver,picFlile,picName_list_back,funcName+"åˆ·å¡åˆ—è¡¨_")
 
-            picName_list_back = u"±¦±¦¿¼ÇÚ-·µ»Ø.png"
+            picName_list_back = u"å®å®è€ƒå‹¤-è¿”å›.png"
             backButton(driver,picFlile,picName_list_back,funcName)
         except:
-            logging.error(u"½øÈë±¦±¦¿¼ÇÚÊ§°Ü")
+            logging.error(u"è¿›å…¥å®å®è€ƒå‹¤å¤±è´¥")
